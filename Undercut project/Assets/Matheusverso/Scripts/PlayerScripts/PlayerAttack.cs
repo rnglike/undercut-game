31831a820a70af435bd780adad7b78cbdc4a6aea
dd2t <54781089+dd2t@users.noreply.github.com>
2020-03-03 12:18:20 -0300

    Provisório semana 2
    Add simple hp system
    Add drop items
    Add simple wepon
    Add Hair 1, stop the time
    Add bugs, drop item can fuck with the enemies physics

---
+++ Undercut project/Assets/Matheusverso/Scripts/PlayerScripts/PlayerAttack.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    // Attack timer
+    private float timeBtwAttack;
+    public float defaultTimeBtwAttack;
+
+
+    // Attack settings
+    public int damage;
+    public Transform attackPosition;
+    public LayerMask whatIsEnemy;
+    public float attackRangeX;
+    public float attackRangeY;
+
+    //Player lock
+    private bool playerLock;
+
+
+    // Animations
+    private Animator playerAnimator;
+
+
+    void Start()
+    {
+        // Geting animator
+        playerAnimator = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        // Is player locked?
+        playerLock = GetComponent<PlayerController>().getPlayerLock();
+
+        // Attack !
+        if (Input.GetKey("k") && timeBtwAttack <= 0 && playerLock == false)
+        {
+            // Maybe put an delay, if necessary
+            timeBtwAttack = defaultTimeBtwAttack;
+            Collider2D[] enemiesToHit = Physics2D.OverlapBoxAll(attackPosition.position, new Vector2(attackRangeX, attackRangeY), 0, whatIsEnemy);
+            for (int i = 0; i < enemiesToHit.Length; i++)
+            {
+                enemiesToHit[i].GetComponent<Enemy>().takeDamage(damage);
+            }
+            Debug.Log("Dealing tons of DAMAGE");
+            playerAnimator.SetTrigger("Attack");
+        } else 
+        {
+            timeBtwAttack = timeBtwAttack - Time.deltaTime;
+        }
+    }
+
+
+    // Drawing the attack range
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireCube(attackPosition.position, new Vector3(attackRangeX, attackRangeY, 1));
+    }
+}
