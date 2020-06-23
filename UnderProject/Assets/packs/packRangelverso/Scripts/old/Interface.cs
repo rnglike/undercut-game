c384b2999e3b28c62bee0df0b5100f115d63d603
Lucas <58084632+sbohfm@users.noreply.github.com>
2020-06-22 21:13:36 -0300

    Hahan't

--- UnderProject/Assets/packs/packRangelverso/Scripts/old/Interface.cs
+++ UnderProject/Assets/packs/packRangelverso/Scripts/old/Interface.cs
@@ -7,17 +7,42 @@ using TMPro;
 
 public class Interface : MonoBehaviour
 {
-	public GameObject cam;
-	public GameObject UI;
+	public TextMeshProUGUI[] life;
+	public TextMeshProUGUI[] levelCurrent; 
+	public TextMeshProUGUI[] levelAmount;
 
-	public bool firstTime;
+	public BuildingNew building;
+	public Playerlifes playerLifes;
 
-	void Start()
+	void Update()
 	{
-		if(firstTime)
+		foreach(TextMeshProUGUI TMP in life)
 		{
-			firstTime = false;
-			cam.GetComponent<CameraZoom>().aproximated = false;
+			TMP.text = playerLifes.lifes.ToString();
+		}
+
+		foreach(TextMeshProUGUI TMP in levelCurrent)
+		{
+			string prefix = "";
+			
+			if(building.currentRoom < 10)
+			{
+				prefix = "0";
+			}
+
+			TMP.text = (prefix + (building.levelToMake - building.currentRoom).ToString());
+		}
+
+		foreach(TextMeshProUGUI TMP in levelAmount)
+		{
+			string prefix = "/";
+
+			if(building.levelToMake < 10)
+			{
+				prefix = "/0";
+			}
+
+			TMP.text = (prefix + building.levelToMake.ToString());
 		}
 	}
 }
