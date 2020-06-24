38b99e19951f8aecbb7f778a3e93c37b368e8521
Lucas <58084632+sbohfm@users.noreply.github.com>
2020-06-24 20:29:56 -0300

    UPDAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAATE

--- UnderProject/Assets/packs/packRangelverso/Scripts/BombTrigger.cs
+++ UnderProject/Assets/packs/packRangelverso/Scripts/BombTrigger.cs
@@ -23,13 +23,13 @@ public class BombTrigger : MonoBehaviour
 
 			if(!activated)
 			{
-				transform.GetChild(1).GetChild(1).gameObject.SetActive(false);
-				transform.GetChild(1).GetChild(0).gameObject.SetActive(true);
+				transform.GetChild(0).GetChild(1).gameObject.SetActive(false);
+				transform.GetChild(0).GetChild(0).gameObject.SetActive(true);
 			}
 			else
 			{
-				transform.GetChild(1).GetChild(1).gameObject.SetActive(true);
-				transform.GetChild(1).GetChild(0).gameObject.SetActive(false);
+				transform.GetChild(0).GetChild(1).gameObject.SetActive(true);
+				transform.GetChild(0).GetChild(0).gameObject.SetActive(false);
 			}
 	    }
 		else
